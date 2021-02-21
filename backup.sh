1162ae9b456cf3a0a0b7ed9319e7faca55907652
Nikolai Chirkov <qwertymonstro@gmail.com>
2021-02-21 18:45:17 +0300

    conditional statements

--- backup.sh
+++ backup.sh
@@ -1,30 +1,42 @@
 #!/bin/bash
 
-# This bash script is used to backup a user's home directory to /tmp/.
-
 user=$(whoami)
 input=/home/$user
 output=/tmp/${user}_home_$(date +%Y-%m-%d_%H%M%S).tar.gz
 
-# The function total_files reports a total number of files for a given directory. 
 function total_files {
         find $1 -type f | wc -l
 }
 
-# The function total_directories reports a total number of directories
-# for a given directory. 
 function total_directories {
         find $1 -type d | wc -l
 }
 
+function total_archived_directories {
+        tar -tzf $1 | grep  /$ | wc -l
+}
+
+function total_archived_files {
+        tar -tzf $1 | grep -v /$ | wc -l
+}
+
 tar -czf $output $input 2> /dev/null
 
-echo -n "Files to be included:"
-total_files $input
-echo -n "Directories to be included:"
-total_directories $input
+src_files=$( total_files $input )
+src_directories=$( total_directories $input )
 
-echo "Backup of $input completed!"
+arch_files=$( total_archived_files $output )
+arch_directories=$( total_archived_directories $output )
 
-echo "Details about the output backup file:"
-ls -l $output
+echo "Files to be included: $src_files"
+echo "Directories to be included: $src_directories"
+echo "Files archived: $arch_files"
+echo "Directories archived: $arch_directories"
+
+if [ $src_files -eq $arch_files ]; then
+        echo "Backup of $input completed!"
+        echo "Details about the output backup file:"
+        ls -l $output
+else
+        echo "Backup of $input failed!"
+fi
